438f1d25fff3b886f6da2f5a9569f02339cee944
Jim White <Jim.White@me.com>
2022-06-27 13:20:43 -0400

    pre-merge post-conflict resolution commit

--- client/reducers/customerReducer.js
+++ client/reducers/customerReducer.js
@@ -1,16 +1,21 @@
 import * as types from '../actions/actions.js';
 
 const initialState = {
-  firstName: 'Jimbo',
-  lastName: 'Whsdfsdfsite',
-  address: '6 Moultie St',
-  city: 'Boston',
-  st: 'MA',
-  zip: '02124',
-  phone: '(857) 212-1143',
-  email: 'jim.white@me.com',
-  id: '',
-};
+  ticketRowElements: [{ticket_id: 1, ticket_reason: 'Internet too slow', 
+      ticket_status: 'Open', first_name: 'John', last_name: 'Smith', create_tmstmp: '2022-06-24 12:00:00'}],
+}
+
+// const initialState = {
+//   firstName: 'Jimbo',
+//   lastName: 'Whsdfsdfsite',
+//   address: '6 Moultie St',
+//   city: 'Boston',
+//   st: 'MA',
+//   zip: '02124',
+//   phone: '(857) 212-1143',
+//   email: 'jim.white@me.com',
+//   id: '',
+// };
 
 // const initialState = {
 //   firstName: '',
@@ -25,19 +30,42 @@ const initialState = {
 // }
 
 const customerReducer = (state = initialState, action) => {
-  console.log(
-    '🔴🟠🟡🟢🔵🟣 | file: customerReducer.js | line 28 | customerReducer | state',
-    state
-  );
-  return state;
-  switch (action.type) {
-    case types.addTicket: {
-      return state;
+  // const clientReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.ADD_TICKET: {
+            return {
+                ...state
+            }
+        }
+
+        case types.ADD_TICKET_ROWS: {
+            return {
+                ...state,
+                ticketRowElements: action.payload,
+            }
+        }
+        // case types.DELETE_TICKET: {
+
+        // }
+        // case types.ESCALATE_TICKET: {
+
+        // }
+        // case types.RESOLVE_TICKET: {
+
+        // }
+        // case types.ADD_CLIENT: {
+
+        // }
+        // case types.ADD_CUSTOMER: {
+
+        // }
+        // case types.ADD_CUSTOMER_PHONE: {
+
+        // }
+        default: {
+            return state
+        }
     }
-    default: {
-      return state;
-    }
-  }
-};
+}
 
 export default customerReducer;
